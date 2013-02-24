b046f579459580a37caa05b692fcc6ffbd0f235f
christian.dollosa@gmail.com <christian.dollosa@gmail.com>
2013-02-24 22:34:48 +0800

    Build new application

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+OrgPortal::Application.config.secret_token = '9ec700cb30ee8967ca9ce3c2b214eef32818911e3c4bbcd9dc562fc95edddffdfbd9155b9f2b771b6dd1d85de4109aa4b41e039f85e96a1c88e46988d4f42148'
